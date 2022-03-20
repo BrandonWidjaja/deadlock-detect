f10a78788d9a11fa2a3e3549515f10226d9d93b5
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2022-03-20 17:41:55 +1100

    implemented task 2

--- detect.c
+++ detect.c
@@ -3,26 +3,32 @@
 #include <stdio.h>
 #include <string.h>
 
+// Definition of a process
 typedef struct {
     int pID;
     int fLock;
     int fReq;
 } node;
 
-void findDistinct(size_t p_len, node* processes);
+void findDistinct(size_t p_len, node* processes); // Print number of distinct processes and files
+int computeExecuteTime(size_t p_len, node* proccesses);
+
 
 int main(int argc, char* argv[]) {
     
-    int args;
-    int filearg=1;
+    int args, filearg = 1;
 
-    bool fFlag=false;
+    // Flags
+    bool fFlag = false, eFlag = false;
 
-    for (args=0; args<argc; args++){
+    for (args = 0; args < argc; args++){
         if (strcmp("-f", argv[args]) == 0){
-            fFlag=true;
+            fFlag = true;
             //printf("file read\n");
             filearg = ++args;
+        }
+        if (strcmp("-e", argv[args]) == 0){
+            eFlag = true;
         }
     }
 
@@ -41,6 +47,7 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Input into struct adapted from COMP30023_2022_SM1 C File IO Introduction (v2) University of Melbourne
     int col1, col2, col3;
     while (fscanf(fp, "%d %d %d", &col1, &col2, &col3) == 3) {
         if (p_len == p_size) {
@@ -63,8 +70,11 @@ int main(int argc, char* argv[]) {
         printf("%d %d %d\n", proccess.pID, proccess.fLock, proccess.fReq);
     } */
 
-    if (fFlag==true){
+    if (fFlag == true){
         findDistinct(p_len, proccesses);
+    }
+    if (eFlag == true){
+        printf("Execution time %d\n", computeExecuteTime(p_len, proccesses));
     }
 
     free(proccesses);
@@ -74,40 +84,56 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
+// Print number of distinct processes and files
 void findDistinct(size_t p_len, node* proccesses){
-    int pNum=0, fNum=0, fCount=0, fAll[p_len*2];
-
+    int fNum = 0, fCount = 0, fAll[p_len*2];
+    
+    // Add all files to fAll array
     for (int i = 0; i < p_len; i++){
         node proccess = proccesses[i];
-        int j = 0;
-
-        while (j != i && proccesses[j].pID != proccess.pID){
-            ++j;
-        }
-        if (j==i){
-            ++pNum;
-        }
-
         fAll[fCount] = proccess.fLock;
         ++fCount;
         fAll[fCount] = proccess.fReq;
         ++fCount;
     }
 
-    for (int i=0; i<fCount; i++){
+    // Count number of distinct files in fAll
+    for (int i = 0; i < fCount; i++){
         int j = 0;
         while (j != i && fAll[j] != fAll[i]){
             ++j;
         }
-        if (j==i){
+        if (j == i){
             ++fNum;
         }
-
-        
     }
-    printf("Processes %d\nFiles %d\n", pNum, fNum);
 
-   
-
+    printf("Processes %zu\nFiles %d\n", p_len, fNum);
 }
 
+// Find the minimum execution time
+int computeExecuteTime(size_t p_len, node* proccesses){
+    int LOCKTIME = 1; // Locking and releasing all files costs 1 time unit
+    int maxReqCount = 1;
+
+    if(p_len < 1){
+        return 0;
+    }
+
+    for (int i = 0; i < p_len; i++){
+        int currReqCount = 1;
+        
+        for (int j = i + 1; j < p_len; j++){
+            // Find count of request of highest frequency
+            if(proccesses[j].fReq == proccesses[i].fReq){
+                ++currReqCount;
+                if (currReqCount > maxReqCount){
+                  maxReqCount = currReqCount;  
+                }
+            }
+        }
+        currReqCount = 0;
+    }
+
+    return maxReqCount + LOCKTIME;
+}
