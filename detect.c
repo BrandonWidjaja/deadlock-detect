bc0ffbf1356d1f74f06067dd8de6b0b446eca210
BrandonWidjaja <w1djaja.brand0n1@gmail.com>
2022-03-22 17:05:28 +1100

    added linked lists implementation

--- detect.c
+++ detect.c
@@ -8,10 +8,27 @@ typedef struct {
     int pID;
     int fLock;
     int fReq;
-} node;
+    bool read;
+} process;
 
-void findDistinct(size_t p_len, node* processes); // Print number of distinct processes and files
-int computeExecuteTime(size_t p_len, node* proccesses);
+/* Structure of a node */
+struct node {
+    int id, count, end;          // Data 
+    struct node *next; // Address 
+}*head;
+
+/* 
+ * Functions to create and display list
+ */
+struct node* add(int i, int id, struct node *newNode, struct node *temp);
+void createList(size_t size, int startID);
+void traverseList();
+void freeList(struct node* head);
+struct node* find(int id);
+bool detectCycle();
+
+void findDistinct(size_t p_len, process* processes); // Print number of distinct processes and files
+int computeExecuteTime(size_t p_len, process* proccesses);
 
 
 int main(int argc, char* argv[]) {
@@ -41,7 +58,7 @@ int main(int argc, char* argv[]) {
 
     // Read file
     size_t p_len = 0, p_size = 2;
-    node* proccesses = malloc(sizeof(node) * p_size);
+    process* proccesses = malloc(sizeof(process) * p_size);
     if (proccesses == NULL) {
         fprintf(stderr, "Malloc failure\n");
         exit(EXIT_FAILURE);
@@ -52,7 +69,7 @@ int main(int argc, char* argv[]) {
     while (fscanf(fp, "%d %d %d", &col1, &col2, &col3) == 3) {
         if (p_len == p_size) {
             p_size *= 2;
-            proccesses = realloc(proccesses, sizeof(node) * p_size);
+            proccesses = realloc(proccesses, sizeof(process) * p_size);
             if (proccesses == NULL) {
                 fprintf(stderr, "Realloc failure\n");
                 exit(EXIT_FAILURE);
@@ -60,13 +77,15 @@ int main(int argc, char* argv[]) {
         }   
         proccesses[p_len].pID = col1;
         proccesses[p_len].fLock = col2;
+        proccesses[p_len].read = false;
         proccesses[p_len++].fReq = col3;
+
     }
 
     // Print nodes 
     /*
     for (int i = 0; i < p_len; i++) {
-        node proccess = proccesses[i];
+        process proccess = proccesses[i];
         printf("%d %d %d\n", proccess.pID, proccess.fLock, proccess.fReq);
     } */
 
@@ -77,6 +96,9 @@ int main(int argc, char* argv[]) {
         printf("Execution time %d\n", computeExecuteTime(p_len, proccesses));
     }
 
+    createList(p_len, proccesses[1].pID);
+    traverseList();
+
     free(proccesses);
     fclose(fp);
 
@@ -85,12 +107,12 @@ int main(int argc, char* argv[]) {
 }
 
 // Print number of distinct processes and files
-void findDistinct(size_t p_len, node* proccesses){
+void findDistinct(size_t p_len, process* proccesses){
     int fNum = 0, fCount = 0, fAll[p_len*2];
     
     // Add all files to fAll array
     for (int i = 0; i < p_len; i++){
-        node proccess = proccesses[i];
+        process proccess = proccesses[i];
         fAll[fCount] = proccess.fLock;
         ++fCount;
         fAll[fCount] = proccess.fReq;
@@ -112,7 +134,7 @@ void findDistinct(size_t p_len, node* proccesses){
 }
 
 // Find the minimum execution time
-int computeExecuteTime(size_t p_len, node* proccesses){
+int computeExecuteTime(size_t p_len, process* proccesses){
     int LOCKTIME = 1; // Locking and releasing all files costs 1 time unit
     int maxReqCount = 1;
 
@@ -137,3 +159,131 @@ int computeExecuteTime(size_t p_len, node* proccesses){
 
     return maxReqCount + LOCKTIME;
 }
+
+/*
+ * Create a list of n nodes
+ */
+void createList(size_t size, int startID){
+    struct node *newNode, *temp;
+    int id, i;
+
+    head = (struct node *)malloc(sizeof(struct node));
+
+    // Terminate if memory not allocated
+    if(head == NULL)
+    {
+        printf("Unable to allocate memory.");
+        exit(0);
+    }
+
+    head->id = startID; // Link data field with data
+    head->count = 0; 
+    head->end = 0;
+    head->next = NULL; // Link address field to NULL
+
+
+    // Create n - 1 nodes and add to list
+    temp = head;
+    for(i=2; i<=size; i++){
+      
+        temp = add(i, i, newNode, temp);
+        printf("\n");
+        /*if (temp->id == 5){
+            temp->next = find(3);
+            temp->end = 1;
+        }*/
+    }
+
+
+}
+
+struct node* add(int i, int id, struct node *newNode, struct node *temp){
+    newNode = (struct node *)malloc(sizeof(struct node));
+
+    newNode->id = id; // Link data field of newNode
+    newNode->count = 0;
+    newNode->next = NULL; // Make sure new node points to NULL 
+
+    temp->next = newNode; // Link previous node with newNode
+    traverseList();
+  
+    return temp->next;    // Make current node as previous node
+  
+}
+
+/*
+ * Display entire list
+ */
+void traverseList()
+{
+    struct node *temp;
+
+    // Return if list is empty 
+    if(head == NULL){
+        printf("List empty.");
+        return;
+    }
+    
+    temp = head;
+    while(temp != NULL){
+        printf("%d (%d) -> ", temp->id, temp->count); // Print data of current node
+        temp = temp->next;                 // Move to next node
+    }
+}
+
+void freeList(struct node* head){
+   struct node* tmp;
+   while (head != NULL && head->end != 1){
+       tmp = head;
+       head = head->next;
+       free(tmp);
+    }
+}
+
+struct node* find(int id) {
+
+   //start from the first link
+   struct node* current = head;
+
+   //if list is empty
+   if(head == NULL) {
+      return NULL;
+   }
+
+   //navigate through list
+   while(current->id != id) {
+        //if it is last node
+        if(current->next == NULL) {
+            return NULL;
+        } else {
+         //go to next link
+            current = current->next;
+        }
+   }      
+	
+   //if data found, return the current Link
+   return current;
+}
+
+bool detectCycle(){
+    struct node *temp;
+
+    // Return if list is empty 
+    if(head == NULL){
+        return false;
+    }
+    
+    temp = head;
+    while(temp != NULL && temp->count != 2){
+        temp->count = temp->count + 1; // Print data of current node
+        printf("%d\n", temp->count);
+
+        if (temp->count == 2){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+
